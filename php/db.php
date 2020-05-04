423eaaad41dba3dd3f7c7eb53aa1948b90b355e1
Vito Laminafra <vitolam@users.noreply.github.com>
2020-05-04 15:31:55 +0200

    Fix for localhost

--- php/db.php
+++ php/db.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $username = "vitolam";
-    $password = "";
+    $username = "root";
+    $password = "root";
 
     $conn = new mysqli($servername, $username, $password, "my_ecity");
     if(!$conn) {
